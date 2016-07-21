a70b5a0b13636739fe03a6f9de58f20b4a826ee9
Mayeul d'Avezac <m.davezac@ucl.ac.uk>
2016-07-21 12:02:07 +0100

    Adding $HOME/lib, /usr/lib, /usr/local/lib to fallback dyld

--- funwith/project.jinja.lua
+++ funwith/project.jinja.lua
@@ -9,7 +9,10 @@ setenv("CURRENT_FUN_WITH_DIR", srcdir)
 {% endif -%}
 
 prepend_path("CMAKE_PREFIX_PATH", homedir)
-prepend_path("DYLD_LIBRARY_PATH", pathJoin(homedir, "lib"))
+prepend_path("DYLD_FALLBACK_LIBRARY_PATH", pathJoin(os.getenv("HOME"), "/lib"))
+prepend_path("DYLD_FALLBACK_LIBRARY_PATH", "/usr/lib")
+prepend_path("DYLD_FALLBACK_LIBRARY_PATH", "/usr/local/lib")
+prepend_path("DYLD_FALLBACK_LIBRARY_PATH", pathJoin(homedir, "lib"))
 prepend_path("PATH", pathJoin(homedir, "bin"))
 
 {% if julia_package_dir -%}
